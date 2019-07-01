6aa1be6e9b7c84fe4188bc8f5a9dc2a5acebc2f0
dmitrie43 <dmitrie43@mail.ru>
2019-07-01 18:58:05 +0400

    Begin

---
+++ application/config/routes.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+
+    '' => [
+        'controller' => 'main',
+        'action' => 'index',
+    ],
+
+    'account/login' => [
+        'controller' => 'account',
+        'action' => 'login',
+    ],
+];
